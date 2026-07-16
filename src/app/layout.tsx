986014ff647845eb6ba37afcb196dd383eb99420
Avinash175 <avinashmagar15@gmail.com>
2026-07-16 11:28:23 +0530

    fix: set default theme to light and disable system theme preference in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,8 +31,8 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col font-sans">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
+          enableSystem={false}
           disableTransitionOnChange
         >
           <CurrencyProvider>
